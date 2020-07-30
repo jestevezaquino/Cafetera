2b747528b5d15a9c195f68c00de73b7db9fc6a5c
Pedro Sosa <psosa@pixnabilab.com>
2020-07-29 21:44:02 -0400

    fit: se completaron los test para cafetera.

---
+++ Cafetera/Class/Cafetera.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cafetera.Class
+{
+    public class Cafetera
+    {
+        public int cantidadCafe { get; set; }
+        public Cafetera(int cantidadCafe) {
+            this.cantidadCafe=cantidadCafe;
+        }
+        public bool hasCafe(int cantidadCafe) {
+            if (cantidadCafe<=this.cantidadCafe)
+            {
+                return true;
+            }
+            return false;
+        }
+        public void giveCafe(int cantidadCafe) {
+            this.cantidadCafe -= cantidadCafe;
+        }
+    }
+}
